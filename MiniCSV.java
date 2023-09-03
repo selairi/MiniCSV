1dd280bfb7029896e1629c1ae1609aa145344f7a
P. L. Lucas <selairi@gmail.com>
2023-09-03 09:06:10 +0200

    Use buffers.

--- MiniCSV.java
+++ MiniCSV.java
@@ -1,3 +1,5 @@
+package scv_backup.app;
+
 import java.util.*;
 import java.io.*;
 
@@ -39,12 +41,15 @@ import java.io.*;
 /** Read and write CSV (comma-separated values) files. There a some functions to builds maps using a column values as key.
 */
 public class MiniCSV {
+
+  final static String CSV_DEFAULT_ENCODING = "UTF-8";
+
   /** The same as readCSV(fileName, "UTF-8").
    * @param fileName The name of CSV file.
    * @return List with CSV table values
    */
   public static List<List<String>> readCSV(String fileName) throws IOException, FileNotFoundException {
-    return readCSV(fileName, "UTF-8");
+    return readCSV(fileName, CSV_DEFAULT_ENCODING);
   }
 
   /** Read the CSV fileName with given encoding, delimiter = , and quotechar = ".
@@ -126,7 +131,8 @@ public class MiniCSV {
    * @param quotechar CSV file quote
    * @param delimiter CSV file delimiter
    */
-  public static void writeCSV(List<List<String>> rows, PrintStream out, char quotechar, char delimiter) {
+  public static void writeCSV(List<List<String>> rows, BufferedWriter out, char quotechar, char delimiter) throws java.io.IOException {
+    //out = new PrintWriter(new BufferedWriter(out));
     int maxCols = 0;
     for(List<String> row : rows)
       if(maxCols < row.size()) maxCols = row.size();
@@ -135,15 +141,15 @@ public class MiniCSV {
         if(n < row.size()) {
           String col = row.get(n);
           boolean quoted = col.contains("" + delimiter) || col.contains(" ") || col.contains("\n");
-          if(quoted) out.print(quotechar);
+          if(quoted) out.write(quotechar);
           col = col.replaceAll("" + quotechar, quotechar + "" + quotechar);
-          out.print(col);
-          if(quoted) out.print(quotechar);
+          out.write(col);
+          if(quoted) out.write(quotechar);
         }
         if(n < (maxCols - 1))
-          out.print(delimiter);
+          out.write(delimiter);
       }
-      out.println();
+      out.write("\n");
     }
   }
 
@@ -151,7 +157,7 @@ public class MiniCSV {
    * @param rows List with data
    * @param output stream to write data
    */
-  public static void writeCSV(List<List<String>> rows, PrintStream out) {
+  public static void writeCSV(List<List<String>> rows, BufferedWriter out) throws java.io.IOException {
     writeCSV(rows, out, '"', ',');
   }
 
@@ -160,8 +166,8 @@ public class MiniCSV {
    * @param fileName File name
    * @param encoding Could be "utf-8", "latin1",...
    */
-  public static void writeCSV(List<List<String>> rows, String fileName, String encoding) throws FileNotFoundException,UnsupportedEncodingException {
-    PrintStream out = new PrintStream(fileName, encoding);
+  public static void writeCSV(List<List<String>> rows, String fileName, String encoding) throws FileNotFoundException,UnsupportedEncodingException, java.io.IOException {
+    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), encoding));
     writeCSV(rows, out, '"', ',');
     out.close();
   }
@@ -170,15 +176,15 @@ public class MiniCSV {
    * @param rows List with data to save
    * @param fileName File name
    */
-  public static void writeCSV(List<List<String>> rows, String fileName) throws FileNotFoundException,UnsupportedEncodingException {
-    writeCSV(rows, fileName, "UTF-8");
+  public static void writeCSV(List<List<String>> rows, String fileName) throws FileNotFoundException,UnsupportedEncodingException,java.io.IOException {
+    writeCSV(rows, fileName, CSV_DEFAULT_ENCODING);
   }
 
   /** Prints rows list to stdout with CSV format
    * @param rows List with data
    */
-  public static void print(List<List<String>> rows) {
-    writeCSV(rows, System.out, '"', ',');
+  public static void print(List<List<String>> rows) throws java.io.IOException {
+    writeCSV(rows, new BufferedWriter(new OutputStreamWriter(System.out)), '"', ',');
   }
 
   /** Builds a HashMap with "list" data using "idColumn" as key and key duplicates are ignored. idColumn have to be a primary key.
@@ -297,8 +303,8 @@ public class MiniCSV {
     List<List<String>> rows = readCSV("example1.csv");
     // rows.remove(0); // Remove first line
     System.out.println("After");
-    //print(rows);
-    writeCSV(rows, System.out);
+    print(rows);
+    //writeCSV(rows, new PrintWriter(System.out));
     System.out.println("Item (2,2): " + rows.get(1).get(1));
     HashMap<Integer, List<String>> map = listToMapIntId(rows, 2, true);
     System.out.println("Item with id 2: " + map.get(2));
